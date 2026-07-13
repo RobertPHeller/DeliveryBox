9f2f2e5d4c1b0b7fe7885f23d5a0173fb1632a14
Robert Heller <heller@deepsoft.com>
2026-07-13 15:18:28 -0400

    Snapshot commit.

--- MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
+++ MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    var deliveryBoxManager = DeliveryBoxBLE()
     @State var ssid = ""
     @State var password = ""
     @State var masterCode = ""
@@ -15,36 +16,40 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
           Text("Delivery Box App").font(.title)
-          Section(header: Text("Connection")) {
-             VStack {
-               HStack {
-                 Button("Connect to Delivery Box") { }
-                 Button("Disconnect from Delivery Box") { }
-               }
-               Text("*BLE State*")
-             }
-          }
           Section(header: Text("WiFi Configuration")) {
              VStack {
                TextField("SSID", text: $ssid)
                SecureField("Password", text: $password)
-               Button("Configure WiFi") { }
+               Button("Configure WiFi",
+                      action: {
+                        deliveryBoxManager.SendWiFiConfigure(ssid: ssid,
+                                                             password: password)
+                      }) 
              }
            }
            Section(header: Text("Set Master Code")) {
              VStack {
                TextField("Master Code:", text: $masterCode)
-               Button("Set Master Code") { }
+               Button("Set Master Code",
+                        action: {
+                            deliveryBoxManager.SendMasterCode(masterCode: masterCode)
+                        }) 
              }
            }
            Section(header: Text("Add One Time Code")) {
              VStack {
                TextField("One Time Code:", text: $oneTimeCode)
-               Button("Add One Time Code") { }
+               Button("Add One Time Code",
+                        action: {
+                            deliveryBoxManager.SendOneTimeCode(oneTimeCode: oneTimeCode)
+                        }) 
              }
            }
            Section(header: Text("Reboot")) {
-             Button("Reboot") { }
+             Button("Reboot",
+                    action: {
+                        deliveryBoxManager.SendReboot()
+                    }) 
            }
         }   
     }
