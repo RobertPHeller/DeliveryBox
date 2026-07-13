9c3e62dec6dd97a087f1af4bd8f4b3e508e22f20
Robert Heller <heller@deepsoft.com>
2026-07-13 11:24:42 -0400

    Basic UI done.

--- MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
+++ MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var ssid = ""
+    @State var password = ""
+    @State var masterCode = ""
+    @State var oneTimeCode = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 12) {
+          Text("Delivery Box App").font(.title)
+          Section(header: Text("Connection")) {
+             VStack {
+               HStack {
+                 Button("Connect to Delivery Box") { }
+                 Button("Disconnect from Delivery Box") { }
+               }
+               Text("*BLE State*")
+             }
+          }
+          Section(header: Text("WiFi Configuration")) {
+             VStack {
+               TextField("SSID", text: $ssid)
+               SecureField("Password", text: $password)
+               Button("Configure WiFi") { }
+             }
+           }
+           Section(header: Text("Set Master Code")) {
+             VStack {
+               TextField("Master Code:", text: $masterCode)
+               Button("Set Master Code") { }
+             }
+           }
+           Section(header: Text("Add One Time Code")) {
+             VStack {
+               TextField("One Time Code:", text: $oneTimeCode)
+               Button("Add One Time Code") { }
+             }
+           }
+           Section(header: Text("Reboot")) {
+             Button("Reboot") { }
+           }
+        }   
     }
 }
 
