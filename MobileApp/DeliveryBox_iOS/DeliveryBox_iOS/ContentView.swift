20031dcb1c5d685ae9cb015e1340d7589e2d33dc
Robert Heller <heller@deepsoft.com>
2026-07-13 20:48:25 -0400

    iOS app basically complete.

--- MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
+++ MobileApp/DeliveryBox_iOS/DeliveryBox_iOS/ContentView.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
+// ObservableObject that limits text fields to numbers (digits) only
+class NumbersOnly: ObservableObject {
+    @Published var value = "" {
+        didSet {
+            let filtered = value.filter { $0.isNumber }
+                                    
+            if value != filtered {
+                value = filtered
+            }
+        }
+    }
+}
+
+
 struct ContentView: View {
+    // Connect to the BLE interface module
     var deliveryBoxManager = DeliveryBoxBLE()
+    let characterLimit = 8
+    // Variables to hold input values
     @State var ssid = ""
     @State var password = ""
-    @State var masterCode = ""
-    @State var oneTimeCode = ""
+    @ObservedObject var masterCode = NumbersOnly()
+    @ObservedObject var oneTimeCode = NumbersOnly()
     var body: some View {
         VStack(alignment: .leading, spacing: 12) {
           Text("Delivery Box App").font(.title)
@@ -29,7 +46,13 @@ struct ContentView: View {
            }
            Section(header: Text("Set Master Code")) {
              VStack {
-               TextField("Master Code:", text: $masterCode)
+               TextField("Master Code:", text: $masterCode.value)
+                          .keyboardType(.decimalPad)
+                          .onChange(of: text) { newValue in
+                              if newValue.count > characterLimit {
+                                  text = String(newValue.prefix(characterLimit))
+                              }
+                           }
                Button("Set Master Code",
                         action: {
                             deliveryBoxManager.SendMasterCode(masterCode: masterCode)
@@ -38,7 +61,13 @@ struct ContentView: View {
            }
            Section(header: Text("Add One Time Code")) {
              VStack {
-               TextField("One Time Code:", text: $oneTimeCode)
+               TextField("One Time Code:", text: $oneTimeCode.value)
+                          .keyboardType(.decimalPad)
+                          .onChange(of: text) { newValue in
+                              if newValue.count > characterLimit {
+                                  text = String(newValue.prefix(characterLimit))
+                              }
+                           }
                Button("Add One Time Code",
                         action: {
                             deliveryBoxManager.SendOneTimeCode(oneTimeCode: oneTimeCode)
@@ -48,10 +77,11 @@ struct ContentView: View {
            Section(header: Text("Reboot")) {
              Button("Reboot",
                     action: {
-                        deliveryBoxManager.SendReboot()
+                     "   deliveryBoxManager.SendReboot()
                     }) 
            }
-        }   
+        }
+        .padding()
     }
 }
 
