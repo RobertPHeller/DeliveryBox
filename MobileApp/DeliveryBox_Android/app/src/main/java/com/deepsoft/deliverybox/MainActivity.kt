76b3b9c212b27a3ee358f2c434dbb2034ab3b137
Robert Heller <heller@gollum3.deepsoft.com>
2026-07-24 12:40:12 -0400

    Mostly Completed the Android UI.

---
+++ MobileApp/DeliveryBox_Android/app/src/main/java/com/deepsoft/deliverybox/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.deepsoft.deliverybox
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView;
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
